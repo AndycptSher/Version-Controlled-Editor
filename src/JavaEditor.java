956a042060f80b9bad88e1e58a6551d514fd54ae
Andy <119109174+AndycptSher@users.noreply.github.com>
2024-07-23 16:23:04 +0100

    Update JavaEditor.java
    added "x" button
    testing with file IO

--- src/JavaEditor.java
+++ src/JavaEditor.java
@@ -1,21 +1,94 @@
-package src;
+package JavaEditor.src;
 
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.plaf.basic.BasicButtonUI;
 import javax.swing.text.JTextComponent;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class JavaEditor extends JFrame{
     
     public JavaEditor(){
+        //Create a panel and add components to it.
+        // JPanel contentPane = new JPanel(new BorderLayout());
+        // contentPane.add(new Button(), BorderLayout.CENTER);
+        // contentPane.add(new Button(), BorderLayout.PAGE_END);
         JTabbedPane contentPane = new JTabbedPane();
         this.setContentPane(contentPane);
         
-        this.setSize(new Dimension(400, 200));
+        initMenuBar();
+        this.setSize(new Dimension(300, 200));
         this.setVisible(true);
+    }
+
+    private void initMenuBar(){
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        JMenu menu = new JMenu("File");
+        menuBar.add(menu);
+        
+        JMenuItem open = new JMenuItem("Open file");
+        open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK)); // binds to CTRL + O
+        open.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                UIManager.put("FileChooser.readOnly", Boolean.TRUE); // stop user from being able to remane files, placed before file selector creation
+                JFileChooser chooser = new JFileChooser();
+                chooser.setAcceptAllFileFilterUsed(false);
+                chooser.setFileFilter(new FileNameExtensionFilter("TEXT FILES", "txt", "text"));
+                
+                switch(chooser.showOpenDialog(null)){
+                    case JFileChooser.APPROVE_OPTION:
+                        File selectedFile = chooser.getSelectedFile();
+                        System.out.println(selectedFile.getName());
+                        // selectedFile;
+                        // FileReader
+                        // BufferedReader;
+                        Scanner scanner;
+                        try{
+                            scanner = new Scanner(selectedFile);
+                        }
+                        catch(FileNotFoundException err){
+                            System.err.print("File: "+selectedFile.getName()+" Not Found");
+                            break;
+                        }
+                        while (scanner.hasNextLine()){
+                            System.out.println(scanner.nextLine());
+                        }
+                        break;
+                    // case JFileChooser:
+                    //     break;
+                }
+                
+                
+            }
+        });
+        menu.add(open);
+
+        JMenuItem close = new JMenuItem("Close Current Tab");
+        close.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, InputEvent.CTRL_DOWN_MASK)); // binds CTRL + W
+        close.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                JTabbedPane contentPane = ((JTabbedPane)JavaEditor.this.getContentPane());
+                contentPane.remove(contentPane.getSelectedComponent());
+            }
+        });
+        menu.add(close);
     }
 
     public void addTab(EditorSpace component){
@@ -26,6 +99,17 @@ public class JavaEditor extends JFrame{
     
     public static void main(String[] args){
         JavaEditor display = new JavaEditor();
+        
+        JTextArea panel0 = new JTextArea();
+        JEditorPane panel1;
+        panel1 = new JEditorPane();
+        JTextPane panel2 = new JTextPane();
+        JTextComponent[] panels = new JTextComponent[]{panel0, panel1, panel2};
+        for (int i=0; i<panels.length;i++){
+            JTextComponent p = panels[i];
+            display.addTab(new EditorSpace(String.valueOf(i), p));
+        }
+
         display.addTab(new EditorSpace("heyyy", new JEditorPane()));
         System.out.println("Hello world!");
     }
@@ -38,6 +122,7 @@ class EditorSpace extends JPanel{
     String name;
     JTextComponent component;
     EditorSpace(String name, JTextComponent component){
+        setLayout(new BorderLayout());
         this.name = name;
         this.component = component;
         add(component);
@@ -49,13 +134,40 @@ class TabComponent extends JPanel{
      * component created at top of tab
      * giving x to close tab feature
      */
-    final JTabbedPane parent;
+    private final JTabbedPane parent;
+    private final deleteButton button;
+
+    private final static MouseListener mouseListener = new MouseAdapter() {
+        
+    public void mouseEntered(MouseEvent e){
+        Component component = e.getComponent();
+        if(component instanceof deleteButton){
+            ((deleteButton)component).setText("⬤");
+        }
+    }
+
+    public void mouseExited(MouseEvent e){
+        Component component = e.getComponent();
+        if(component instanceof deleteButton){
+                ((deleteButton)component).setText("x");
+            }
+        }
+        
+    };
     private class deleteButton extends JButton{
         public deleteButton(){
             super("x");
+            setPreferredSize(new Dimension(20, 20));
             setToolTipText("Close this tab");
+            setUI(new BasicButtonUI());
+            // transparent
             setContentAreaFilled(false);
+            setFocusable(false);
+            // makes x visible instead of ...
+            setBorder(new BevelBorder(ABORT));
             setBorderPainted(false);
+            this.addMouseListener(mouseListener);
+            // setRolloverEnabled(true);
             addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e){
@@ -87,8 +199,8 @@ class TabComponent extends JPanel{
             }
         });
         
-        // JButton deleteTabButton = new deleteButton();
-        this.add(new deleteButton());
+        this.button = new deleteButton();
+        this.add(this.button);
         this.setOpaque(false);
     }
     private int getTabIndex(){

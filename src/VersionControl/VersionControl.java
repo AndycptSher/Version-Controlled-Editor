9911c6248753915d11143f1ba0b7771ddd7a2a40
CocytusDEDI <cocytusdedi@tutanota.com>
2024-08-04 15:33:55 +0100

    Starting work on version control

--- src/VersionControl/VersionControl.java
+++ src/VersionControl/VersionControl.java
@@ -1,13 +1,63 @@
 package VersionControl;
 
-public class VersionControl{
-    private String file_path;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-    public VersionControl(String file_path) {
-        this.file_path = file_path;
+public class VersionControl {
+    private Path filePath;
+    private Path versionControlPath;
+
+    public VersionControl(String filePath) {
+        this.filePath = Paths.get(filePath);
+        this.versionControlPath = this.filePath.getParent().resolve("." + this.filePath.getFileName() + "_version_control");
+        createVersionControlFolder();
     }
 
-    public String get_file_path() {
-        return file_path;
+    public String getFilePath() {
+        return filePath.toString();
+    }
+
+    public String getVersionControlPath() {
+        return versionControlPath.toString();
+    }
+
+    public void save() {
+
+    }
+
+    private String getFileHash() throws IOException {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+        } catch (NoSuchAlgorithmException _error) {
+            System.err.println("SHA-1 hashing algorithm couldn't be found");
+            System.exit(1);
+        }
+        /*
+        FileInputStream fileInputSteam = new FileInputStream(filePath);
+
+        byte[] buffer = new byte[1024];
+
+        int bytesRead = 0;
+        */
+
+        return "";
+    }
+
+    private void createVersionControlFolder() {
+        if (Files.notExists(versionControlPath)) {
+            try {
+                Files.createDirectories(versionControlPath);
+                Files.createDirectories(versionControlPath.resolve("versions"));
+                Files.createFile(versionControlPath.resolve("branches"));
+            } catch (IOException _error) {
+                System.err.println("Couldn't create version control folder");
+            }
+        }
     }
 }
+
